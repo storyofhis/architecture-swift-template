5784f9a12f0f1aca4d122730c64f10dbaa5d18b3
storyofhis <azizi.maula@gmail.com>
2026-05-15 15:33:34 +0700

    Initial Commit

---
+++ architecture-swift-template/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  architecture-swift-template
+//
+//  Created by Maula Izza Azizi on 15/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ architecture-swift-template/architecture_swift_templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  architecture_swift_templateApp.swift
+//  architecture-swift-template
+//
+//  Created by Maula Izza Azizi on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct architecture_swift_templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
